10b30bae9d5f8cf312615535b86d066b03c579b5
Sebastien Dru <sebdru.fr@gmail.com>
2023-01-02 12:07:00 +0100

    fix bootstrap form implementation, rm translation, rm symfony-extra-bundle

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,16 +5,13 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Exception\ORMException;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation;
 
 class UserController extends AbstractController
 {
