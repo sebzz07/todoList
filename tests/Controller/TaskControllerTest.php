ed721af68a8a4cb171e0c2dcfefb4e9e896f9dc6
Sébastien Dru <sebdru.fr@gmail.com>
2023-03-28 18:08:45 +0200

    set phpunit to exclude DataFixture and Security, add last tests to obtain 85% coverage

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace App\Tests\Controller;
 
+use App\Entity\Task;
 use App\Entity\User;
 use App\Repository\TaskRepository;
 use App\Repository\UserRepository;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
 class TaskControllerTest extends WebTestCase
 {
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function testUsersListAuthorizedForAdmin(): void
     {
@@ -39,7 +41,7 @@ class TaskControllerTest extends WebTestCase
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function testUsersListUnauthorizedForUser(): void
     {
@@ -47,6 +49,7 @@ class TaskControllerTest extends WebTestCase
         $userRepository = static::getContainer()->get(UserRepository::class);
 
         // retrieve the test user
+        /** @var User $testUser */
         $testUser = $userRepository->findOneBy(['username' => 'user1']);
 
         // simulate $testUser being logged in
@@ -64,7 +67,7 @@ class TaskControllerTest extends WebTestCase
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function testUsersNotAllowedToAccessTasksOfAnotherUser(): void
     {
@@ -72,6 +75,7 @@ class TaskControllerTest extends WebTestCase
         $userRepository = static::getContainer()->get(UserRepository::class);
 
         // retrieve the test user
+        /** @var User $testUser */
         $testUser = $userRepository->findOneBy(['username' => 'user1']);
 
         // simulate $testUser being logged in
@@ -84,7 +88,7 @@ class TaskControllerTest extends WebTestCase
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function testUsersEditTaskWithSuccess(): void
     {
@@ -94,6 +98,7 @@ class TaskControllerTest extends WebTestCase
         $taskRepository = static::getContainer()->get(TaskRepository::class);
 
         // retrieve the test user
+        /** @var User $testUser */
         $testUser = $userRepository->findOneBy(['username' => 'user1']);
 
         // simulate $testUser being logged in
@@ -114,7 +119,7 @@ class TaskControllerTest extends WebTestCase
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function testUsersToggleTaskWithSuccess(): void
     {
@@ -122,6 +127,7 @@ class TaskControllerTest extends WebTestCase
         $userRepository = static::getContainer()->get(UserRepository::class);
 
         // retrieve the test user
+        /** @var User $testUser */
         $testUser = $userRepository->findOneBy(['username' => 'user1']);
 
         // simulate $testUser being logged in
@@ -134,27 +140,66 @@ class TaskControllerTest extends WebTestCase
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function testUsersNotAllowedToToggleTasksOfAnotherUser(): void
     {
         $client = static::createClient();
         $userRepository = static::getContainer()->get(UserRepository::class);
 
+        // retrieve the test userAdmin
+        /** @var User $testUserA */
+        $testUserA = $userRepository->findOneBy(['username' => 'user1']);
+
         // retrieve the test user
-        $testUser = $userRepository->findOneBy(['username' => 'user1']);
+        /** @var User $testUserB */
+        $testUserB = $userRepository->findOneBy(['username' => 'user2']);
+
+        /** @var Task $task */
+        $task = $testUserB->getTasks()->first();
 
         // simulate $testUser being logged in
-        $client->loginUser($testUser);
-        $crawler = $client->request('GET', '/tasks/22/toggle');
+        $client->loginUser($testUserA);
+        $client->request('GET', '/tasks/'.$task->getId().'/toggle');
         $client->followRedirect();
         $this->assertResponseIsSuccessful();
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
-        $this->assertSelectorTextContains('strong', 'Oops !');
+        $this->assertSelectorTextContains('html div.alert-danger', "Oops ! Vous ne possédez pas de droit suffisant pour changer cette tâche");
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
+     */
+    public function testUserAdminNotAllowedToToggleTasksOfAnotherUser(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // retrieve the test userAdmin
+        /** @var User $testUserAdmin */
+        $testUserAdmin = $userRepository->findOneBy(['username' => 'admin1']);
+
+
+        // retrieve the test user
+        /** @var User $testUser */
+        $testUser = $userRepository->findOneBy(['username' => 'user1']);
+
+        /** @var Task $task */
+        $task = $testUser->getTasks()->first();
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUserAdmin);
+        $client->request('GET', '/tasks/'.$task->getId().'/toggle');
+        $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorTextContains('html div.alert-danger', "Oops ! Vous ne pouvez pas changer la tâche d'un autre utilisateur");
+
+    }
+
+
+    /**
+     * @throws Exception
      */
     public function testUserDeleteHisTask(): void
     {
@@ -165,15 +210,93 @@ class TaskControllerTest extends WebTestCase
         // retrieve the test user
         /** @var User $testUser */
         $testUser = $userRepository->findOneBy(['username' => 'user1']);
+
+        /** @var Task $task */
         $task = $testUser->getTasks()->first();
 
         // simulate $testUser being logged in
         $client->loginUser($testUser);
         $client->request('GET', '/tasks/'.$task->getId().'/delete');
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorExists('div.success', 'La tâche a bien été supprimée.');
+        $this->assertSelectorTextContains('html div.alert-success', "La tâche a bien été supprimée.");
 
-        // $ModifiedTask = $taskRepository->findOneBy(['id' => $task->getId()]);
-        // $this->assertTrue($ModifiedTask);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testUserAdminDeleteHisTask(): void
+    {
+        $client = static::createClient();
+        $client->followRedirects();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // retrieve the test userAdmin
+        /** @var User $testUserAdmin */
+        $testUserAdmin = $userRepository->findOneBy(['username' => 'admin1']);
+
+        /** @var Task $task */
+        $task = $testUserAdmin->getTasks()->first();
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUserAdmin);
+        $client->request('GET', '/tasks/'.$task->getId().'/delete');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('html div.alert-success', "La tâche a bien été supprimée.");
+
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testUserDeleteTaskOfAnOtherUser(): void
+    {
+        $client = static::createClient();
+        $client->followRedirects();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // retrieve the test userAdmin
+        /** @var User $testUserA */
+        $testUserA = $userRepository->findOneBy(['username' => 'user1']);
+
+        // retrieve the test user
+        /** @var User $testUserB */
+        $testUserB = $userRepository->findOneBy(['username' => 'user2']);
+
+        /** @var Task $task */
+        $task = $testUserB->getTasks()->first();
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUserA);
+        $client->request('GET', '/tasks/'.$task->getId().'/delete');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('html div.alert-danger', "Oops ! Vous ne pouvez supprimer que vos propres tâches");
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testAdminDeleteTaskOfAnOtherUser(): void
+    {
+        $client = static::createClient();
+        $client->followRedirects();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // retrieve the test userAdmin
+        /** @var User $testUserAdmin */
+        $testUserAdmin = $userRepository->findOneBy(['username' => 'admin1']);
+
+        // retrieve the test user
+        /** @var User $testUser */
+        $testUser = $userRepository->findOneBy(['username' => 'user1']);
+
+        /** @var Task $task */
+        $task = $testUser->getTasks()->first();
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUserAdmin);
+        $client->request('GET', '/tasks/'.$task->getId().'/delete');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('html div.alert-danger', "Oops ! Vous ne pouvez pas supprimer la tâche d'un autre utilisateur");
     }
 }
